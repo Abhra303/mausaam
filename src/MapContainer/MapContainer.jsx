703a0d3ece8236732f91946e1a2ff1909bdd6a1c
Subhra264 <chakrabortysubhradeep556@gmail.com>
2021-12-05 16:53:12 +0530

    Add Mapbox event listeners, add marker

--- src/MapContainer/MapContainer.jsx
+++ src/MapContainer/MapContainer.jsx
@@ -1,8 +1,9 @@
 import { useState } from 'react';
-import ReactMapGl from 'react-map-gl';
+import ReactMapGl, { Marker } from 'react-map-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import Snackbar from '@mui/material/Snackbar';
 import { Alert } from '@mui/material';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
 
 const MAPBOX_TOKEN = process.env.REACT_APP_MAPBOX_TOKEN || '';
 console.log('MAPBOX_TOKEN', MAPBOX_TOKEN);
@@ -36,6 +37,16 @@ export default function MapContainer (props) {
         });
     };
 
+    const onClickOnMap = (pointerEvent) => {
+        if (pointerEvent.type === 'click') {
+            console.log('Pointer event on click', pointerEvent);
+            // props.setMarkerLocation({
+            //     longitude: pointerEvent.lngLat[0],
+            //     latitude: pointerEvent.lngLat[1]
+            // });
+        }
+    };
+
     return (
         <div style={{ width: '100%', height: '100%' }}>
             <ReactMapGl 
@@ -46,7 +57,19 @@ export default function MapContainer (props) {
                 mapStyle={'mapbox://styles/subhra264/ckwszma1h2e2215nv4gcm4pn3'}
                 onViewportChange={setViewport}
                 onError={showError}
-            />
+                onClick={onClickOnMap}
+            >
+                
+                {
+                    props.markerLocation && 
+                    <Marker 
+                        latitude={props.markerLocation.latitude} 
+                        longitude={props.markerLocation.longitude}
+                    >
+                        <LocationOnIcon sx={{ color: 'red' }} />
+                    </Marker>
+                }
+            </ReactMapGl>
             <Snackbar
                 open={toastStatus.open}
                 autoHideDuration={5000}
